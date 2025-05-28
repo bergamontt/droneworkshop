dab5c6d3ad39ce61f4bd57f8b5342f764647ffdd
Stellar-Nucleosynthesis <stepanenkonazar356@gmail.com>
2025-05-28 16:59:49 +0300

    Minor design update (searchbar removed)

--- frontend/src/pages/Tutorials.jsx
+++ frontend/src/pages/Tutorials.jsx
@@ -1,7 +1,6 @@
 import { useFetch } from '../hooks/useFetch.jsx';
 import { getAllTutorialCategories } from '../services/TutorialCategoryService.jsx';
 import { getAllTutorials } from '../services/TutorialService.jsx';
-import Searchbar from "../components/common/Searchbar.jsx";
 import TutorialsList from '../components/common/TutorialsList.jsx';
 import '../styles/Tutorials.css'
 
@@ -19,16 +18,11 @@ function Tutorials() {
     if (!categories || !tutorials) return(<h1>Error Occurred!</h1>);
 
     return(
-
         <section className='tutorials-page-container'>
-
             <article className='tutorials-main-container' style={mainContainerStyles}>
-                <Searchbar placeholder="Search"/>
                 <TutorialsList categories={categories} tutorials={tutorials} />
             </article>
-
         </section>
-
     );
 }
 
